a2d24279be984af63dab14ca0976ded894984848
Michael Schams <typo3@2019.schams.net>
2019-01-22 08:36:53 +1100

    [TASK] Initial commit for version 3 re-development
    Version 3 of the TYPO3 Nagios extension is a complete re-development
    for TYPO3 v9 LTS and higher. Compatibility with older versions of
    TYPO3 CMS has been dropped.
    
    This is the initial commit.

---
+++ README.md
@@ -0,0 +1,37 @@
+# TYPO3 Extension: Nagios
+
+Nagios TYPO3 Monitoring (TYPO3 extension "nagios")
+
+## Description
+
+Nagios is the industry standard in open source server/service/network monitoring.
+This TYPO3 CMS extension gathers information about the TYPO3 instance for a Nagios monitoring server.
+
+## Documentation
+
+A comprehensive documentation is available at [docs.typo3.org](https://docs.typo3.org/typo3cms/extensions/nagios).
+
+Find out more about the TYPO3 entprise content management system at: https://typo3.org
+
+## Installation
+
+Please see the [documentation](https://docs.typo3.org/typo3cms/extensions/nagios) for detailed installation instruction.
+
+## System Requirements
+
+* TYPO3 v9 LTS
+* PHP version 7.2
+
+## License
+
+(c) 2010-2019 Michael Schams (schams.net), all rights reserved
+
+This software free software; you can redistribute it and/or modify it under the terms of the GNU General Public License, either version 2 of the License, or any later version. For the full copyright and license information, please see the documentation that was distributed with this source code.
+
+The GNU General Public License can be found at:
+http://www.gnu.org/copyleft/gpl.html
+
+
+Author
+------
+Michael Schams <schams.net>
---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the TYPO3 CMS Extension "Nagios TYPO3 Monitoring"
+ *
+ * Author: Michael Schams <schams.net>
+ * Website: https://schams.net
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Nagios TYPO3 Monitoring',
+    'description' => 'Monitors TYPO3 instances and warns about insecure extensions, old TYPO3 versions,
+        wrong PHP versions, etc. Requires a Nagios monitoring server. Detailed documentation, screenshots
+        and more at: http://schams.net/nagios',
+    'category' => 'misc',
+    'version' => '3.0.0',
+    'state' => 'beta',
+    'createDirs' => '',
+    'clearcacheonload' => true,
+    'author' => 'Michael Schams (schams.net)',
+    'author_email' => 'schams.net',
+    'author_company' => 'https://schams.net',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.2.0-7.2.99',
+            'typo3' => '9.5.0-9.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
