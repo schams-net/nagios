d0888b01d51333928f3afae3f5b6568336eb2396
Michael Schams <typo3@2019.schams.net>
2019-01-30 10:18:31 +1100

    [TASK] Re-implement access checks
    This update completes the checks, if the HTTP request originated from a
    client, that is allowed to access details of the TYPO3 instance. If
    configured in the extension configuration, proxy servers are taken into
    account.
    
    Resolves #17

--- Classes/Utility/AccessUtility.php
+++ Classes/Utility/AccessUtility.php
@@ -45,31 +45,55 @@ class AccessUtility
     private const INVALID_IP_ADDRESSES = ['*.*.*.*', '0.0.0.0'];
 
     /**
-     * Returns ...
-     * ...
-     * @param string ...
-     * @param bool ...
-     * @param ServerRequestInterface ...
-     * @return bool ...
+     * Checks, if the HTTP request originated from a client, that is
+     * allowed to access details of the TYPO3 instance. The "client"
+     * is usually a Nagios server.
+     *
+     * @param string Comma-separated list of IP addresses and hostnames (as configured)
+     * @param bool If set to "true", proxy servers are taken into account
+     * @param ServerRequestInterface PSR-7 server request interface
+     * @return bool Returns "true", if the requests originates from a valid client, otherwise "false"
      */
-    public static function isValidClient(
-        string $securityNagiosServerList,
-        bool $takeProxyServerIntoAccount,
-        ServerRequestInterface $request
-    ) {
+    public static function isValidClient(string $serverList, bool $proxyServer, ServerRequestInterface $request): bool
+    {
+        /** @var NormalizedParams $normalizedParams */
         $normalizedParams = $request->getAttribute('normalizedParams');
         $remoteAddress = $normalizedParams->getRemoteAddress();
 
-        if (isset($securityNagiosServerList)
-            && !empty($securityNagiosServerList)
-            && is_string($securityNagiosServerList)) {
+        if (isset($serverList)
+            && !empty($serverList)
+            && is_string($serverList)) {
             // Make sure, extension configuration only contains valid entries
             // and hostnames are resolved to IP addresses
-            $securityNagiosServerList = self::resolveHostnamesInList($securityNagiosServerList);
+            $serverList = self::resolveHostnamesInList($serverList);
 
             // check remote IP address
-            if (GeneralUtility::cmpIP($remoteAddress, $securityNagiosServerList) === true) {
+            if (GeneralUtility::cmpIP($remoteAddress, $serverList) === true) {
                 return true;
+            }
+
+            // Check if HTTP headers exist, which are typical for a proxy/cache/load balancer.
+            if ($proxyServer === true) {
+                $serverParams = $request->getServerParams();
+                foreach ($serverParams as $httpHeaderKey => $httpHeaderValue) {
+                    $httpHeaderKey = preg_replace('/^HTTP_/', '', trim(strtoupper($httpHeaderKey)));
+                    if (is_string($httpHeaderKey) && !empty($httpHeaderKey)
+                        && in_array($httpHeaderKey, self::VALID_PROXY_HEADERS)
+                        && is_string($httpHeaderValue) && !empty($httpHeaderValue)
+                    ) {
+                        // Allow for comma-separated list of multiple IP addresses,
+                        // for example: "123.10.10.10, 123.10.10.20"
+                        $candidates = explode(',', $httpHeaderValue);
+                        $candidates = array_map('trim', $candidates);
+
+                        foreach ($candidates as $candidate) {
+                            if (GeneralUtility::validIPv4($candidate)
+                            && GeneralUtility::cmpIP($candidate, $serverList) === true) {
+                                return true;
+                            }
+                        }
+                    }
+                }
             }
         }
         return false;
@@ -82,7 +106,7 @@ class AccessUtility
      * @param string Comma-separated list of IP addresses and hostnames (wildcard allowed)
      * @return string Comma-separated list of IP addresses (wildcard allowed)
      */
-    public static function resolveHostnamesInList($list)
+    public static function resolveHostnamesInList(string $list): string
     {
         $resolvedList = [];
         $list = GeneralUtility::trimExplode(',', $list, true);
@@ -93,7 +117,8 @@ class AccessUtility
                     $resolvedList[] = $item;
                 }
             } else {
-                // Convert hostname to IP address
+                // Convert hostname to IP address: gethostbynamel() returns a list of IPv4
+                // addresses to which the Internet host specified by hostname resolves.
                 $hosts = gethostbynamel($item);
                 if ($hosts) {
                     $resolvedList = array_merge($resolvedList, $hosts);
