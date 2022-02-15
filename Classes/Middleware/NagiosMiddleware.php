1450d5de1c5d8e8ddcc009106342253182f7f72e
Michael Schams <noreply@2022.schams.net>
2022-02-15 14:27:47 +1100

    [TASK] Simplify PSR-15 middleware
    The PSR-7 server request interface allows us to access the request URI
    directly. This change eliminates the $normalizedParams->getRequestUri()
    call and simplifies the middleware class.

--- Classes/Middleware/NagiosMiddleware.php
+++ Classes/Middleware/NagiosMiddleware.php
@@ -34,26 +34,16 @@ class NagiosMiddleware implements MiddlewareInterface
 {
     /**
      * Extension key
-     *
-     * @access private
-     * @var string
      */
-    private $extensionKey = 'nagios';
+    private string $extensionKey = 'nagios';
 
     /**
      * Extension configuration object
-     *
-     * @access private
-     * @var ExtensionConfiguration;
      */
-    private $extensionConfiguration;
+    private ExtensionConfiguration $extensionConfiguration;
 
     /**
      * Dispatches the request to the corresponding typoscript_rendering configuration
-     *
-     * @param ServerRequestInterface $request Server request interface
-     * @param RequestHandlerInterface $handler Request handler interface
-     * @return ResponseInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
@@ -69,23 +59,16 @@ class NagiosMiddleware implements MiddlewareInterface
 
     /**
      * Checks if the request is for the TYPO3 Nagios extension
-     *
-     * @access private
-     * @param ServerRequestInterface $request PSR-7 server request interface
-     * @return bool
      */
     private function isNagiosRequest(ServerRequestInterface $request): bool
     {
-        /** @var NormalizedParams $normalizedParams */
-        $normalizedParams = $request->getAttribute('normalizedParams');
-        $requestUri = self::sanitizeString($normalizedParams->getRequestUri());
-
         // Extract configured URI from extension configuration (e.g. "/nagios")
         $this->extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class);
         $expectedUri = $this->extensionConfiguration->get($this->extensionKey, 'expectedUri');
         $expectedUri = self::sanitizeString($expectedUri);
 
-        if (preg_match('/^\/[a-zA-Z0-9\/\-\_]{3,}/', $expectedUri) && $requestUri === $expectedUri) {
+        // Check if the request is a request for the Nagios extension
+        if (preg_match('/^\/[a-zA-Z0-9\/\-\_]{3,}/', $expectedUri) && $request->getRequestTarget() === $expectedUri) {
             return true;
         }
 
@@ -94,10 +77,6 @@ class NagiosMiddleware implements MiddlewareInterface
 
     /**
      * Sanitize string
-     *
-     * @access private
-     * @param string $uri Uniform resource identifier (URI) to sanitize
-     * @return string
      */
     private static function sanitizeString(string $uri): string
     {
